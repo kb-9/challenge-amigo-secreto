6421bd4081ac1e238fdc9168b02d4d58ea82b75d
Karla <kb._@outlook.com>
2025-02-20 23:02:57 -0500

    funcion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nombreDeAmigo = document.getElementById('amigo'); 
+    
+    //Validacion de entrada
+    if (nombreDeAmigo.value === ''){
+        alert('Por favor, inserte un nombre');
+    } else {
+        amigos.push(nombreDeAmigo);
+    }
+
+    //Limpiar caja
+    nombreDeAmigo.value = '';
+}
