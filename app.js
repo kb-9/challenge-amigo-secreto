93063765284550f1d6ffb3a1563cf3c24886fb1e
Karla <kb._@outlook.com>
2025-02-21 01:02:32 -0500

    Muestra lista amigos y actualizacion de funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+
 function agregarAmigo(){
     let nombreDeAmigo = document.getElementById('amigo'); 
     
     //Validacion de entrada
     if (nombreDeAmigo.value === ''){
         alert('Por favor, inserte un nombre');
+    } else if (amigos.includes(nombreDeAmigo.value)){  //evita duplicados
+        alert('El amigo ya fue ingresado');
+        limpiarCaja();
     } else {
-        amigos.push(nombreDeAmigo);
+        amigos.push(nombreDeAmigo.value);
+        //Mostrar amigos
+        mostrarAmigos();
+        //Limpiar caja
+        limpiarCaja();
     }
 
-    //Limpiar caja
-    nombreDeAmigo.value = '';
+    return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
+
+function mostrarAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let amigo = 0; amigo < amigos.length; amigo++){
+        lista.innerHTML += `<li>${amigos[amigo]}</li>`;
+    }
 }
